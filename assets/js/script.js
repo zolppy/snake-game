0d87a06aec9af1864f5a17aa87735bd1ee8b2c67
zolppy <gabriel.lcifba@gmail.com>
2024-01-19 23:00:10 -0300

    super refatorado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-/* Adicionar botão de resetar high scores */
-
 const soundButton = document.querySelector("#toggle-mute-sound");
 const canvas = document.querySelector("#stage");
 const context = canvas.getContext("2d");
@@ -10,10 +8,24 @@ const keyRightSound = new Audio("assets/audio/right.mp3");
 const keyLeftSound = new Audio("assets/audio/left.mp3");
 const keyDownSound = new Audio("assets/audio/down.mp3");
 const highScores = [0, 0, 0];
-let box = 32;
+const box = 32;
 const snake = [
-  { x: 8 * box, y: 8 * box, direction: { x: 1, y: 0 } },
-  { x: 16 * box, y: 16 * box, direction: { x: 1, y: 0 } },
+  {
+    x: 8 * box,
+    y: 8 * box,
+    direction: {
+      x: 1,
+      y: 0
+    }
+  },
+  {
+    x: 16 * box,
+    y: 16 * box,
+    direction: {
+      x: 1,
+      y: 0
+    }
+  }
 ];
 
 const sprites = new Image();
@@ -28,40 +40,17 @@ const food = {
 
 const compare = (a, b) => b - a;
 
-const activateSound = () => {
+function toggleActivateSound(action) {
+  let soundOn = (action === "on");
   const soundIcon = document.querySelector("#sound");
-  const soundOff = "assets/img/icons/sound-off.svg";
+  const sounds = [deadSound, eatSound, keyUpSound, keyRightSound, keyLeftSound, keyDownSound];
+
+  soundIcon.src = (soundOn) ? "assets/img/icons/sound-off.svg" : "assets/img/icons/sound-on.svg";
+  soundIcon.alt = "ícone de cor azul que representa som " + ((soundOn) ? "desativado" : "ativado");
+  soundIcon.classList = (soundOn) ? "sound-off" : "sound-on";
   
-  deadSound.muted = false;
-  eatSound.muted = false;
-  keyUpSound.muted = false;
-  keyRightSound.muted = false;
-  keyLeftSound.muted = false;
-  keyDownSound.muted = false;
-
-  soundIcon.src = soundOff;
-  soundIcon.alt = "ícone de cor azul que representa som desativado";
-  soundIcon.classList.replace("sound-on", "sound-off");
-
-  localStorage.setItem("sound-on", JSON.stringify(true));
-}
-
-const muteSound = () => {
-  const soundIcon = document.querySelector("#sound")
-  const soundOn = "assets/img/icons/sound-on.svg";
-  
-  deadSound.muted = true;
-  eatSound.muted = true;
-  keyUpSound.muted = true;
-  keyRightSound.muted = true;
-  keyLeftSound.muted = true;
-  keyDownSound.muted = true;
-
-  soundIcon.src = soundOn;
-  soundIcon.alt = "ícone de cor azul que representa som ativado";
-  soundIcon.classList.replace("sound-off", "sound-on");
-
-  localStorage.setItem("sound-on", JSON.stringify(false));
+  sounds.forEach(sound => sound.muted = !soundOn);
+  localStorage.setItem("sound-on", JSON.stringify(soundOn));
 }
 
 const drawBG = () => {
@@ -76,17 +65,11 @@ function drawSnakeHead() {
     ((snake[0].direction.y === 1)  && { x: 256, y: 64 }) ||
     ((snake[0].direction.y === -1) && { x: 192, y: 0 });
   
-  context.drawImage(
-    sprites,
-    spriteHeadPosition.x, spriteHeadPosition.y,
-    64, 64,
-    snake[0].x, snake[0].y,
-    box, box
-  );
+  context.drawImage(sprites, spriteHeadPosition.x, spriteHeadPosition.y, 64, 64, snake[0].x, snake[0].y, box, box);
 }
 
 function drawSnakeBody() {
-  for (let i = 1, total = snake.length - 1; i < total; i++) {
+  for (let i = 1, length = snake.length - 1; i < length; i++) {
     const prevXNode = snake[i + 1].direction.x * -1;
     const prevYNode = snake[i + 1].direction.y * -1;
   
@@ -103,13 +86,7 @@ function drawSnakeBody() {
       ((right && down)  && { x: 0, y: 0 })     ||
       ((right && up)    && { x: 0, y: 64 });
   
-    context.drawImage(
-      sprites,
-      spriteBodyPosition.x, spriteBodyPosition.y,
-      64, 64,
-      snake[i].x, snake[i].y,
-      box, box
-    );
+    context.drawImage(sprites, spriteBodyPosition.x, spriteBodyPosition.y, 64, 64, snake[i].x, snake[i].y, box, box );
   }
 }
 
@@ -122,13 +99,7 @@ function drawSnakeTail() {
       ((snake[i].direction.y > 0) && { x: 256, y: 192 }) ||
       ((snake[i].direction.y < 0) && { x: 192, y: 128 });
   
-    context.drawImage(
-      sprites,
-      spriteTailPosition.x, spriteTailPosition.y,
-      64, 64,
-      snake[i].x, snake[i].y,
-      box, box
-    );
+    context.drawImage(sprites, spriteTailPosition.x, spriteTailPosition.y, 64, 64, snake[i].x, snake[i].y, box, box);
   }
 }
 
@@ -138,16 +109,7 @@ function drawSnake() {
   drawSnakeTail();
 }
 
-const drawFood = () => {
-  context.drawImage(
-    sprites,
-    0,
-    192,
-    64, 64,
-    food.x, food.y,
-    box, box
-  );
-}
+const drawFood = () => context.drawImage(sprites, 0, 192, 64, 64, food.x, food.y, box, box);
 
 function updateScore() {
   const scoreElement = document.getElementById("score");
@@ -155,19 +117,17 @@ function updateScore() {
   scoreElement.textContent = score;
 }
 
-function updateStorage() {
-  localStorage.setItem("high-scores", JSON.stringify(highScores.sort(compare)));
-}
-
-const KEY_CODES = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-};
+const updateStorage = () => localStorage.setItem("high-scores", JSON.stringify(highScores.sort(compare)));
 
 const keyPress = (event) => {
   const head = snake[0];
+
+  const KEY_CODES = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  };
 
   switch (event.keyCode) {
     case KEY_CODES.LEFT:
@@ -193,7 +153,6 @@ const keyPress = (event) => {
         head.direction = { x: 0, y: 1 };
         keyDownSound.play();
       }
-      break;
   }
 };
 
@@ -209,10 +168,7 @@ const updateScores = () => {
 
 const gameOver = () => {
   clearInterval(game);
-  snake.direction = {
-    x: 0,
-    y: 0,
-  };
+  snake.direction = {};
 
   const reload = () => {
     document.addEventListener("keydown", () => location.reload());
@@ -262,24 +218,32 @@ const gameOver = () => {
 }
 
 function startGame() {
-  if (snake[0].x > 15 * box && snake[0].direction.x == 1) snake[0].x = 0;
-  if (snake[0].x > 15 * box && snake[0].direction.y == -1) snake[0].x = 0;
-  if (snake[0].x > 15 * box && snake[0].direction.y == 1) snake[0].x = 0;
+  if ((snake[0].x > 15 * box && snake[0].direction.x === 1) ||
+      (snake[0].x > 15 * box && snake[0].direction.y === -1) ||
+      (snake[0].x < 0 && snake[0].direction.y === 1)) {
+    snake[0].x = 0
+  }
 
-  if (snake[0].x < 0 && snake[0].direction.x == -1) snake[0].x = 15 * box;
-  if (snake[0].x < 0 && snake[0].direction.y == -1) snake[0].x = 15 * box;
-  if (snake[0].x < 0 && snake[0].direction.y == 1) snake[0].x = 15 * box;
+  if ((snake[0].x < 0 && snake[0].direction.x === -1) ||
+      (snake[0].x < 0 && snake[0].direction.y === -1) ||
+      (snake[0].x < 0 && snake[0].direction.y === 1)) {
+    snake[0].x = 15 * box;
+  }
 
-  if (snake[0].y > 15 * box && snake[0].direction.y == 1) snake[0].y = 0;
-  if (snake[0].y > 15 * box && snake[0].direction.x == 1) snake[0].y = 0;
-  if (snake[0].y > 15 * box && snake[0].direction.x == -1) snake[0].y = 0;
+  if ((snake[0].y > 15 * box && snake[0].direction.y === 1) ||
+      (snake[0].y > 15 * box && snake[0].direction.x === 1) ||
+      (snake[0].y > 15 * box && snake[0].direction.x === -1)) {
+    snake[0].y = 0;
+  }
 
-  if (snake[0].y < 0 && snake[0].direction.y == -1) snake[0].y = 15 * box;
-  if (snake[0].y < 0 && snake[0].direction.x == 1) snake[0].y = 15 * box;
-  if (snake[0].y < 0 && snake[0].direction.x == -1) snake[0].y = 15 * box;
+  if ((snake[0].y < 0 && snake[0].direction.y === -1) ||
+      (snake[0].y < 0 && snake[0].direction.x === 1) ||
+      (snake[0].y < 0 && snake[0].direction.x === -1)) {
+    snake[0].y = 15 * box;
+  }
 
-  for (i = 1; i < snake.length; i++) {
-    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+  for (let i = 1, length = snake.length; i < length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
       deadSound.play();
 
       for (let i = 0; i < 3; i++) {
@@ -291,7 +255,8 @@ function startGame() {
       }
 
       gameOver();
-      setTimeout(function () {
+
+      setTimeout(() => {
         gameOver();
       }, 500);
 
@@ -304,15 +269,17 @@ function startGame() {
   drawSnake();
   drawFood();
 
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
+  let SX = snake[0].x;
+  let SY = snake[0].y;
+  let SDX = snake[0].direction.x;
+  let SDY = snake[0].direction.y;
 
-  if (snake[0].direction.x == 1) snakeX += box;
-  if (snake[0].direction.x == -1) snakeX -= box;
-  if (snake[0].direction.y == -1) snakeY -= box;
-  if (snake[0].direction.y == 1) snakeY += box;
-
-  if (snakeX != food.x || snakeY != food.y) {
+  (SDX === 1) && (SX += box);
+  (SDX === -1) && (SX -= box);
+  (SDY === -1) && (SY -= box);
+  (SDY === 1) && (SY += box);
+  
+  if (SX != food.x || SY != food.y) {
     snake.pop();
   } else {
     score++;
@@ -332,11 +299,12 @@ function startGame() {
   }
 
   let newHead = {
-    x: snakeX,
-    y: snakeY,
+    x: SX,
+    y: SY,
+
     direction: {
-      x: snake[0].direction.x,
-      y: snake[0].direction.y,
+      x: SDX,
+      y: SDY,
     },
   };
 
@@ -346,7 +314,7 @@ function startGame() {
 soundButton.addEventListener("click", () => {
   const soundIcon = document.querySelector("#sound");
 
-  soundIcon.classList.contains("sound-off") ? muteSound() : activateSound();
+  soundIcon.classList.contains("sound-off") ? toggleActivateSound("off") : toggleActivateSound("on");
 });
 
 document.addEventListener("keydown", keyPress);
@@ -354,25 +322,9 @@ document.addEventListener("keydown", keyPress);
 window.addEventListener("load", () => {
   const thisScores = JSON.parse(localStorage.getItem("high-scores"));
   const sound = JSON.parse(localStorage.getItem("sound-on"));
-  const soundIcon = document.querySelector("#sound");
-
-  const soundOn = "assets/img/icons/sound-on.svg";
-  const soundOff = "assets/img/icons/sound-off.svg";
 
   if (localStorage.getItem("sound-on") !== null) {
-    if (sound) {
-      activateSound();
-      soundIcon.src = soundOff;
-      soundIcon.alt = "ícone de cor azul que representa som desativado";
-      soundIcon.classList.replace("sound-on", "sound-off");
-    }
-
-    if (!sound) {
-      muteSound();
-      soundIcon.src = soundOn;
-      soundIcon.alt = "ícone de cor azul que representa som ativado";
-      soundIcon.classList.replace("sound-off", "sound-on");
-    }
+    sound ? toggleActivateSound("on") : toggleActivateSound("off");
   }
 
   let i = 0;
